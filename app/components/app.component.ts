e6e0ffcf293e5f6da52dc11f3d04c999dc1959ab
S Vinesh <svinesh3691@gmail.com>
2016-11-23 19:33:39 +0530

    Initial commit

---
+++ app/components/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    
+    selector: 'my-app',
+    templateUrl: './app/views/app.html',
+
+})
+
+export class AppComponent {
+  title = 'Angular 2 CURD Application';
+}
